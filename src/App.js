a4032a36d6a188f01db0df0b5949d05198d15e45
Denis Valdivieso <sshshaka@gmail.com>
2021-11-04 08:46:52 -0400

    + Styled Components - src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import styled from "@emotion/styled";
+import image from "./cryptomonedas.png";
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Image = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-bottom: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Crypto</h1>
-    </div>
+    <Container>
+      <div>
+        <Image src={image} alt="Main Image" />
+      </div>
+      <div>
+        <Heading>Cryptocurrency</Heading>
+      </div>
+    </Container>
   );
 }
 
